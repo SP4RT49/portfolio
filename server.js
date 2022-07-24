c3072d7f7b3bd745a9303f7f62cd8d1c5f5f46f6
LETOURNEUR Thibault <thibaukt.letourneur@gmail.com>
2022-07-25 00:17:28 +0200

    [ADD]Folders

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
+const port = 3000;
 
-const PORT = 3000;
+const requestTime = function (req, res, next) {
+    const date = new Date()
+    req.requestTime = date.toLocaleDateString("fr")
+    next()
+  }
+
+  // MIDDLEWARE
+app.use(express.static('public'));
+app.use(requestTime)
 
 
+// -------- Routes
+// Page Home
 app.get('/', (req, res) => {
-    res.send('Api is running')
+    let responseTest = 'Petite message <br>'
+    responseTest += `Dernière visite le ${req.requestTime}`
+    res.send(responseTest)
+})
+// Page login
+    .get('/login', (req, res) => {
+    res.render('login/login.ejs')
 })
 
 
 
-app.listen(process.env.PORT || PORT, () => console.log("Serveur lancé : http://localhost:"+ PORT +"/"))
+app.listen(port, () => console.log("Serveur lancé : http://localhost:"+ port +"/"))
